aa11c246126306a39ba8886127fbf1b2a84e92db
Srijenanithish <srijenanithish@gmail.com>
2021-11-09 11:06:09 +0530

    Loaction enable permission commit

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -76,6 +76,7 @@ class MyHomePage extends State<HomePage> {
                     setState(() {
                       _hasBeenPressed = false;
                     });
+                    _getGeoLocationPosition();
                     _getCurrentLocation();
                   },
                   shape: RoundedRectangleBorder(
@@ -249,10 +250,40 @@ class MyHomePage extends State<HomePage> {
     );
   }
 
+  Future<Position> _getGeoLocationPosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      await Geolocator.openLocationSettings();
+      return Future.error('Location services are disabled.');
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+  }
+
   //Function for Geolocation latitude and longitude
   _getCurrentLocation() {
     Geolocator.getCurrentPosition(
-            desiredAccuracy: LocationAccuracy.best,
+            desiredAccuracy: LocationAccuracy.high,
             forceAndroidLocationManager: true)
         .then((Position position) {
       setState(() {
